f1f4048b8ac8c17f84ddab87a6725dff07182711
oliviercherrier <olivier.cherrier@gmail.com>
2016-12-29 17:29:23 +0100

    Add tree with node from users & Add leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import{ User} from './shared/user';
 import {UsersService} from './shared/users.service';
 import { TreeModule } from 'angular2-tree-component';
 
+import * as L from 'leaflet';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['app/app.component.css'],
   providers: [UsersService],
 
 })
@@ -20,10 +22,10 @@ export class AppComponent {
       this.usersList = r;
       this.nodes = []; 
       for (let userKey in this.usersList){
-        let currentUser = {'id': this.usersList[userKey]._id, "name": this.usersList[userKey].first_name};
+        let currentUser = {'userObject': this.usersList[userKey],'id': this.usersList[userKey]._id, "name": this.usersList[userKey].first_name};
         let currentPaths = [];
         for(let pathKey in this.usersList[userKey].paths){
-          currentPaths.push({'name': this.usersList[userKey].paths[pathKey]._id});
+          currentPaths.push({'pathObject': this.usersList[userKey].paths[pathKey], 'name': this.usersList[userKey].paths[pathKey]._id, 'id': this.usersList[userKey].paths[pathKey]._id});
         }
         currentUser["children"] = currentPaths;
         this.nodes.push(currentUser);
@@ -31,5 +33,22 @@ export class AppComponent {
       console.log(this.nodes);
     }); 
    
+  }
+  ngOnInit(){
+    var myMap = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(myMap);
+
+  }
+
+  onActivate = ($event) => {
+    console.log($event);
+    if ($event.node.data.userObject){
+      // If it is not a path object, do nothing
+      console.log("I am a user Object");
+    }
+    else if ($event.node.data.pathObject){
+      console.log("I am a path Object");
+    }
+    
   }
 }
