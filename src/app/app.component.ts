f842228ae989859749c994aeb9f2a0fe8beb3a18
oliviercherrier <olivier.cherrier@gmail.com>
2016-12-29 19:05:55 +0100

    Update map according to selected path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import{ User} from './shared/user';
+import{ Location} from './shared/location';
 import {UsersService} from './shared/users.service';
 import { TreeModule } from 'angular2-tree-component';
 
@@ -16,6 +17,7 @@ import * as L from 'leaflet';
 export class AppComponent {
   usersList: User[];
   nodes: any[];
+  private map: any;
 
   constructor(private _usersService: UsersService) {
     this._usersService.getUsers().subscribe((r: User[]) => {
@@ -30,25 +32,31 @@ export class AppComponent {
         currentUser["children"] = currentPaths;
         this.nodes.push(currentUser);
       }
-      console.log(this.nodes);
     }); 
    
   }
   ngOnInit(){
-    var myMap = L.map('mapid').setView([51.505, -0.09], 13);
-    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(myMap);
-
+    this.map = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(this.map);
   }
 
   onActivate = ($event) => {
     console.log($event);
     if ($event.node.data.userObject){
       // If it is not a path object, do nothing
-      console.log("I am a user Object");
     }
     else if ($event.node.data.pathObject){
-      console.log("I am a path Object");
+      let leafletLatLong = this.toLeafLetLatLong($event.node.data.pathObject.locations);
+      let leafLetLocation = L.polyline(leafletLatLong, {color: 'red'}).addTo(this.map);
+      this.map.fitBounds(leafLetLocation.getBounds());
     }
-    
+  }
+
+  toLeafLetLatLong = (locations: Location[]): LatLng[] =>{
+    let leafletLatLong: LatLng[] = [];
+    for (let locKey in locations){
+      leafletLatLong.push([locations[locKey].latitude, locations[locKey].longitude])
+    }
+    return leafletLatLong;
   }
 }
