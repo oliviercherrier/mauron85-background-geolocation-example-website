6c23dab56e7192cd47c0836dbedbca6d7b900c9b
oliviercherrier <olivier.cherrier@gmail.com>
2016-12-30 16:31:11 +0100

    Version update

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
 export class Config {
-    static REST_API_ADDRESS = "http://localhost:3000/mauron85-bgeoloc-api";
+    static REST_API_ADDRESS = "http://vps342125.ovh.net:3000/mauron85-bgeoloc-api";
 }
 
